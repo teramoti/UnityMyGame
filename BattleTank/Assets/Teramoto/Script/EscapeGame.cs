3e1f4a7bc1eae4544423058eba1ef94739f25f60
寺本啓志 <teramoti0315@icloud.com>
2019-11-01 08:47:21 +0900

    終了ボタンの追加とアイコンの変更

---
+++ BattleTank/Assets/Teramoto/Script/EscapeGame.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscapeGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Escape"))
+        {
+            ExitGame();
+        }
+    }
+    //終了関数
+    void ExitGame()
+    {
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #elif UNITY_STANDALONE
+            UnityEngine.Application.Quit();
+        #endif
+    }
+}
